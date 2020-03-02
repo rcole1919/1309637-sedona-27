3e0f3421cb154fc295aac9fe94590f4dc6b9bb7e
rcole1919 <roland.alex@mail.ru>
2020-03-02 17:01:37 +0400

    Подготовка к первой проверке

--- js/datepicker.js
+++
@@ -1,9 +0,0 @@
-// Datepicker 
-
-$( function() {
-    $( "#datepicker-1" ).datepicker();
-  } );
-	
-$( function() {
-    $( "#datepicker-2" ).datepicker();
-  } );
---
+++ js/popup-min.js
@@ -0,0 +1,1 @@
+var open=document.querySelector(".search-btn");var popup=document.querySelector(".form");open.addEventListener("click",function(evt){evt.preventDefault();popup.classList.toggle("form-none");});
