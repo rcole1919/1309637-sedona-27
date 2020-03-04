04cb194251934709c8f3387be8c72168196cdaaf
rcole1919 <roland.alex@mail.ru>
2020-03-04 22:41:45 +0400

    Вторая проверка

--- js/popup-min.js
+++ js/popup-min.js
@@ -1,1 +1,1 @@
-var open=document.querySelector(".search-btn");var popup=document.querySelector(".form");open.addEventListener("click",function(evt){evt.preventDefault();popup.classList.toggle("form-none");});
+var open=document.querySelector(".search-btn");var popup=document.querySelector(".form");open.addEventListener("click",function(evt){if(popup.classList.contains("form-none")){evt.preventDefault();popup.classList.remove("form-none");popup.classList.add("form-show");}else{evt.preventDefault();popup.classList.add("form-none");popup.classList.remove("form-show");}});
--- js/popup.js
+++ js/popup.js
@@ -4,8 +4,16 @@ var open = document.querySelector(".search-btn");
 
 var popup = document.querySelector(".form");
 	
+	
 	open.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	popup.classList.toggle("form-none");
-  });
+		if (popup.classList.contains("form-none")) {
+			evt.preventDefault();
+			popup.classList.remove("form-none");
+			popup.classList.add("form-show");
+		} else {
+			evt.preventDefault();
+			popup.classList.add("form-none");
+			popup.classList.remove("form-show");
+		}
+	});
 	
